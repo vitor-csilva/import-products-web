6dc95a5ad0f71e0d7ec847c2548b7a78e62fb019
vitor costa <vitorcs.contato@gmail.com>
2025-01-20 20:29:30 -0300

    Fix: name main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -58,7 +58,7 @@ async def import_files(file: UploadFile):
 
         df = pd.read_csv(file_location)
 
-        if df.columns.tolist() != ['name', 'cost_price', 'sale_price', 'qukantity']:
+        if df.columns.tolist() != ['name', 'cost_price', 'sale_price', 'quantity']:
             raise HTTPException(
                 status_code=500, detail="O arquivo não está no formato correto!")
 
